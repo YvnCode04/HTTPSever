7363b46d20016aa29e6504c028e3a10349de5c6c
Yvan Gauthier <y.gauthier04@icloud.com>
2024-08-06 09:14:13 +0200

    Initial Commit

---
+++ HTTPSever/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HTTPSever
+//
+//  Created by Yvan Gauthier on 06/08/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
